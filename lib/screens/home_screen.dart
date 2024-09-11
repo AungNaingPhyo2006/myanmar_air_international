8d72ddaaf3d13f642804ad1935bcda0a550d3f1a
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-09-11 23:40:23 +0630

    add image and outline styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:myanmar_air_international/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,25 +9,43 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    children: [Text('data'), Text('Hi')],
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 70,
-                  )
-                ],
-              ),
-              const Row(
-                children: [Text('Hello'), Text('Testing')],
-              )
-            ],
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Good Morning',
+                          style: AppStyles.headLineStyle2,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          'Book Tickets',
+                          style: AppStyles.headLineStyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              image: AssetImage("assets/images/logo.png"))),
+                    )
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Search Icon'), Text('Empty Space')],
+                )
+              ],
+            ),
           )
         ],
       ),
